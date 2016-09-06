5ee5e46f10f4df759256e1df7ad5dc6d363fa516
James Fenn <18jafenn90@gmail.com>
2016-09-06 05:42:34 -0400

    possibly fix for issue #20

--- app/src/main/java/com/james/status/data/icon/BatteryIconData.java
+++ app/src/main/java/com/james/status/data/icon/BatteryIconData.java
@@ -58,7 +58,7 @@ public class BatteryIconData extends IconData {
 
             onDrawableUpdate(VectorDrawableCompat.create(getContext().getResources(), getIconResource(iconLevel), getContext().getTheme()));
 
-            if (hasText()) onTextUpdate(String.valueOf(level) + "%");
+            if (hasText()) onTextUpdate(String.valueOf(((double) level / scale) * 100) + "%");
         }
     }
 
@@ -193,7 +193,7 @@ public class BatteryIconData extends IconData {
 
             onDrawableUpdate(VectorDrawableCompat.create(getContext().getResources(), getIconResource(iconLevel), getContext().getTheme()));
 
-            if (hasText()) onTextUpdate(String.valueOf((level / scale) * 100) + "%");
+            if (hasText()) onTextUpdate(String.valueOf(((double) level / scale) * 100) + "%");
         }
     }
 }
