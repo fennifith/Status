130c0fd118d61c3c2dd7f4898ba85a0f45d21f39
James Fenn <18jafenn90@gmail.com>
2017-03-28 19:56:55 -0400

    made the about section readable even without a connection to the inter nets

--- app/src/main/java/com/james/status/activities/AboutActivity.java
+++ app/src/main/java/com/james/status/activities/AboutActivity.java
@@ -1,6 +1,9 @@
 package com.james.status.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -30,8 +33,10 @@ public class AboutActivity extends AppCompatActivity {
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        Drawable errorDrawable = new ColorDrawable(Color.DKGRAY);
+
         jamesImage = (ImageView) findViewById(R.id.jamesImage);
-        Glide.with(this).load("https://theandroidmaster.github.io/images/headers/rocks.jpg").into(jamesImage);
+        Glide.with(this).load("https://theandroidmaster.github.io/images/headers/rocks.jpg").error(errorDrawable).into(jamesImage);
         findViewById(R.id.james).setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -47,13 +52,13 @@ public class AboutActivity extends AppCompatActivity {
 
         findViewById(R.id.jamesTwitter).setOnClickListener(new UrlClickListener("https://twitter.com/IDontLikePHP"));
 
-        Glide.with(this).load("https://theandroidmaster.github.io/images/headers/highway.jpg").into((ImageView) findViewById(R.id.anasImage));
+        Glide.with(this).load("https://theandroidmaster.github.io/images/headers/highway.jpg").error(errorDrawable).into((ImageView) findViewById(R.id.anasImage));
 
         findViewById(R.id.anasGplus).setOnClickListener(new UrlClickListener("https://plus.google.com/101325935187431392674"));
 
         findViewById(R.id.anasTwitter).setOnClickListener(new UrlClickListener("https://twitter.com/MAKTHG"));
 
-        Glide.with(this).load("https://theandroidmaster.github.io/images/headers/vukheader.jpg").into((ImageView) findViewById(R.id.vukImage));
+        Glide.with(this).load("https://theandroidmaster.github.io/images/headers/vukheader.jpg").error(errorDrawable).into((ImageView) findViewById(R.id.vukImage));
 
         findViewById(R.id.vukGplus).setOnClickListener(new UrlClickListener("https://plus.google.com/+Vuka%C5%A1inAn%C4%91elkovi%C4%87zavukodlak"));
 
@@ -61,7 +66,7 @@ public class AboutActivity extends AppCompatActivity {
 
         findViewById(R.id.vukDribbble).setOnClickListener(new UrlClickListener("https://dribbble.com/zavukodlak"));
 
-        Glide.with(this).load("https://technologx.com/android/ghostninja/header.png").into((ImageView) findViewById(R.id.ghostImage));
+        Glide.with(this).load("https://technologx.com/android/ghostninja/header.png").error(errorDrawable).into((ImageView) findViewById(R.id.ghostImage));
 
         findViewById(R.id.ghostWebsite).setOnClickListener(new UrlClickListener("https://technologx.com/"));
 
