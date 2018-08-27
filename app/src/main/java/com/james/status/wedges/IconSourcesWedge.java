fbeb0ef14c6dbb950c9973da6fe8c99de52c49ae
James Fenn <18jafenn90@gmail.com>
2018-08-27 11:45:28 -0400

    add icon sources - #134

---
+++ app/src/main/java/com/james/status/wedges/IconSourcesWedge.java
@@ -0,0 +1,56 @@
+package com.james.status.wedges;
+
+import android.content.Context;
+import android.content.res.XmlResourceParser;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.james.status.R;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import me.jfenn.attribouter.adapters.InfoAdapter;
+import me.jfenn.attribouter.wedges.Wedge;
+
+public class IconSourcesWedge extends Wedge<IconSourcesWedge.ViewHolder> {
+
+    public IconSourcesWedge(XmlResourceParser parser) {
+        super(R.layout.wedge_icon_sources);
+
+        try {
+            Method method = Wedge.class.getDeclaredMethod("addChildren", XmlResourceParser.class);
+            method.setAccessible(true);
+            method.invoke(this, parser);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public ViewHolder getViewHolder(View v) {
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void bind(Context context, ViewHolder viewHolder) {
+        viewHolder.recycler.setLayoutManager(new LinearLayoutManager(context));
+        viewHolder.recycler.setAdapter(new InfoAdapter(getChildren()));
+    }
+
+    public static class ViewHolder extends Wedge.ViewHolder {
+
+        private RecyclerView recycler;
+
+        public ViewHolder(View v) {
+            super(v);
+            recycler = v.findViewById(R.id.recycler);
+        }
+    }
+
+}
