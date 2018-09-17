9c730b39c9b0efdb7c8818ff431c122d9a24ae54
James Fenn <18jafenn90@gmail.com>
2018-09-17 18:11:27 -0400

    add empty view, fix reset functionality

--- app/src/main/java/com/james/status/fragments/AppPreferenceFragment.java
+++ app/src/main/java/com/james/status/fragments/AppPreferenceFragment.java
@@ -2,6 +2,7 @@ package com.james.status.fragments;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
@@ -14,13 +15,10 @@ import android.view.ViewGroup;
 
 import com.james.status.R;
 import com.james.status.adapters.AppAdapter;
-import com.james.status.data.AppData;
 import com.james.status.data.AppPreferenceData;
-import com.james.status.utils.StaticUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class AppPreferenceFragment extends SimpleFragment {
@@ -28,19 +26,22 @@ public class AppPreferenceFragment extends SimpleFragment {
     private RecyclerView recycler;
 
     private AppAdapter adapter;
-    private List<AppData> apps;
-
-    public AppPreferenceFragment() {
-        apps = new ArrayList<>();
-    }
+    private View emptyView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_apps, container, false);
 
+        emptyView = v.findViewById(R.id.empty);
         recycler = v.findViewById(R.id.recycler);
+        recycler.setLayoutManager(new GridLayoutManager(getContext(), 1));
 
+        updateItems();
+        return v;
+    }
+
+    public void updateItems() {
         Map<String, AppPreferenceData> apps = new HashMap<>();
         Map<String, ?> prefs = PreferenceManager.getDefaultSharedPreferences(getContext()).getAll();
         for (String key : prefs.keySet()) {
@@ -54,22 +55,27 @@ public class AppPreferenceFragment extends SimpleFragment {
         }
 
         adapter = new AppAdapter(getContext(), new ArrayList<>(apps.values()));
-        recycler.setLayoutManager(new GridLayoutManager(getContext(), 1));
         recycler.setAdapter(adapter);
 
-        return v;
+        emptyView.setVisibility(apps.size() > 0 ? View.GONE : View.VISIBLE);
     }
 
     public void reset() {
         new AlertDialog.Builder(getContext()).setTitle(R.string.reset_all).setMessage(R.string.reset_apps_confirm).setPositiveButton(R.string.action_ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                for (AppData app : apps) {
-                    app.clearPreferences(getContext());
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+                Map<String, ?> prefs = preferences.getAll();
+                SharedPreferences.Editor editor = preferences.edit();
+                for (String key : prefs.keySet()) {
+                    for (String pref : new String[]{"/APP_COLOR", "/APP_FULLSCREEN", "/APP_FULLSCREEN_IGNORE"}) {
+                        if (key.endsWith(pref))
+                            editor.remove(key);
+                    }
                 }
 
-                StaticUtils.updateStatusService(getContext(), true);
-                adapter.notifyDataSetChanged();
+                editor.apply();
+                updateItems();
                 dialogInterface.dismiss();
             }
         }).setNegativeButton(R.string.action_cancel, new DialogInterface.OnClickListener() {
